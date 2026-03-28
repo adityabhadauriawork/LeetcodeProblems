1bc1fcbbfa948df5db2f7852330772e367a249d2
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-03-28 20:17:28 +0530

    Time: 3 ms (11.2%), Space: 19.6 MB (14.17%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,6 +5,7 @@ class Solution:
             return 1
         #recursive case
         a = self.Pow(x,n//2)
+        # if 2,10 then -> 10//2 = 5 there fore return (2^5)^2 since odd therefore * 2
         if n%2==0:
             return a*a
         else:
