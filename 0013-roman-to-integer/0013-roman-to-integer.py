9f6b865f5a6ee6b55d49ffe8794c1aba703bfbd3
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-04-18 12:09:11 +0530

    Time: 5 ms (52.2%), Space: 19.3 MB (28.17%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        maal = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+
+        }
+        ans = 0
+        for i in range(len(s)):
+            if i < len(s)-1 and maal[s[i]] < maal[s[i+1]]:
+                ans-=maal[s[i]]
+            else:
+                ans+=maal[s[i]]
+        return ans
