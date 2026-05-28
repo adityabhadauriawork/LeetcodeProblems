b7e7d93b8c5f9b33d88de5b34f10a64942a463f6
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-05-28 19:08:14 +0530

    Time: 15 ms (43.24%), Space: 19.5 MB (27.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #bruteforce
+        dict1={}
+        dict2={}
+        for a in s:
+            if a in dict1:
+                dict1[a]+=1
+            else:
+                dict1[a]=1
+        for b in t:
+            if b in dict2:
+                dict2[b]+=1
+            else:
+                dict2[b]=1
+        if dict1==dict2:
+            return True
+        return False
+        
