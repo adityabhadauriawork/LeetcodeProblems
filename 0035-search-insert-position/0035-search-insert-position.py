efaa45e4559988b39c63cd3b005966be09b7b9b6
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-05-28 23:03:25 +0530

    Time: 0 ms (100%), Space: 19.9 MB (42.99%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -8,7 +8,7 @@ class Solution:
             mid = (l+r) // 2
             if nums[mid]>=target:
                 ans = mid
-                r = mid-1
+                r = mid-1 # first greater number milte hi rukna nhi hai , aur check krna hai that's why used
             else:
                 l = mid+1
         return ans
