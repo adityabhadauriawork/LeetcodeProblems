d0653f9ab24d0dd5b2ba71deae599b3345e798d7
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-05-28 20:22:35 +0530

    Time: 7 ms (81.24%), Space: 19.7 MB (43.58%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        new_s = ""
+
+        for ch in s:
+            if ch.isalnum():
+                new_s = new_s + ch.lower()
+
+        rev = ""
+
+        for i in range(len(new_s)-1, -1, -1):
+            rev = rev + new_s[i]
+
+        if new_s == rev:
+            return True
+
+        return False
