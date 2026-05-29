809c8b260de251c1b600515c3c56132b5f61e47b
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-05-29 14:05:10 +0530

    Time: 0 ms (100%), Space: 19.3 MB (57.53%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -8,21 +8,29 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
         # brutalforce solution
-        curr = head
-        counter = 0
+        # curr = head
+        # counter = 0
 
-        while curr != None:
-            curr = curr.next
-            counter = counter + 1
+        # while curr != None:
+        #     curr = curr.next
+        #     counter = counter + 1
 
-        if counter % 2 == 0:
-            counter = (counter // 2)
-        else:
-            counter = (counter // 2)   # CHANGED: removed +1 because it was moving extra step
+        # if counter % 2 == 0:
+        #     counter = (counter // 2)
+        # else:
+        #     counter = (counter // 2)   # CHANGED: removed +1 because it was moving extra step
 
-        curr2 = head                   # CHANGED: removed "head = counter"
+        # curr2 = head                   # CHANGED: removed "head = counter"
 
-        for i in range(counter):       # CHANGED: used loop to move till middle node
-            curr2 = curr2.next
+        # for i in range(counter):       # CHANGED: used loop to move till middle node
+        #     curr2 = curr2.next
 
-        return curr2                   # CHANGED: directly return middle node
+        # return curr2                   # CHANGED: directly return middle node
+
+        #FAST AND SLOW POINTER
+        slow=head
+        fast=head
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
