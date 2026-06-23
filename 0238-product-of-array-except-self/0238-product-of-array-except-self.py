29e6150399590ce4a4345a2e9fb73eb3334e0a71
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-06-24 00:41:23 +0530

    Time: 19 ms (76.07%), Space: 24.9 MB (96.86%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,3 +1,27 @@
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         no_of_zeros=0
+#         n=len(nums)
+#         product = 1
+#         for a in nums:
+#             if a == 0:
+#                 no_of_zeros+=1
+#             elif a!=0:    
+#                 product = product*a
+#         if no_of_zeros > 1:
+#             return [0]*n
+#         answer = []
+#         for i in range(n):
+            
+#             if no_of_zeros == 0:
+#                 answer.append(product//nums[i])
+#             if no_of_zeros == 1:
+#                 if nums[i] != 0:
+#                     answer.append(0)
+#                 elif nums[i]==0:
+#                     answer.append(product)
+#         return answer
+                
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         no_of_zeros=0
@@ -10,16 +34,15 @@ class Solution:
                 product = product*a
         if no_of_zeros > 1:
             return [0]*n
-        answer = []
         for i in range(n):
             
             if no_of_zeros == 0:
-                answer.append(product//nums[i])
+                nums[i] = product//nums[i]
             if no_of_zeros == 1:
                 if nums[i] != 0:
-                    answer.append(0)
+                    nums[i]=0
                 elif nums[i]==0:
-                    answer.append(product)
-        return answer
+                    nums[i]=product
+        return nums
                 
         
