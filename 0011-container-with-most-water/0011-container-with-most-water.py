c18108ba52397181ef7e477a610b623f94390161
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-07-05 09:07:25 +0530

    Time: 55 ms (73.14%), Space: 29.7 MB (36.98%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,29 +1,35 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-        # BRUTE FORCE
         # ans = 0
-        # for l in range(len(height)):
-        #     for r in range(l+1, len(height)):
-        #         area = (r - l) * min(height[l], height[r])
-        #         ans = max(ans,area)
+        # for r in range(len(height)):
+        #     l = r+1
+            
+        #     while l <= len(height) - 1:
+        #         brth = l - r
+        #         leng = min(height[r] , height[l])
+        #         area = leng * brth
+        #         ans = max(ans, area)
+        #         l+=1
+                
         # return ans
 
-        # optimized soltuion two pointer -> to find the big l and breath instead of going on everyone
-
-        left = 0
-        res= 0 
-        right = len(height) - 1
-        while left < right:
-            area = (right - left) * min(height[left] , height[right])
-
-            if height[left] < height[right]:
-                left+=1
+        ###########
+        l = 0
+        n = len(height)
+        r = n-1
+        ans = 0
+        while l<r:
+            area = (r-l) * (min(height[l] , height[r]))
+            ans = max(ans, area)
+            if height[l] < height[r]:
+                l+=1
             else:
-                right -=1
+                r-=1
             
-            res = max(res, area)
-        return res
+        return ans
 
+
+
+        
 
         
