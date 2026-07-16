4fb74f94ac2fc9d80362604b2728a0289cd38054
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-07-16 22:35:29 +0530

    Time: 76 ms (40.84%), Space: 21.4 MB (46.24%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,36 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        rows = len(maze)
+        cols = len(maze[0])
+
+        queue = deque()
+        queue.append((entrance[0], entrance[1], 0))
+
+        visited = set()
+        visited.add((entrance[0], entrance[1]))
+
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        while queue:
+            r, c, steps = queue.popleft()
+
+            # Check if current cell is an exit
+            if (r == 0 or r == rows - 1 or c == 0 or c == cols - 1) and [r, c] != entrance:
+                return steps
+
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+
+                if (0 <= nr < rows and
+                    0 <= nc < cols and
+                    maze[nr][nc] == '.' and
+                    (nr, nc) not in visited):
+
+                    visited.add((nr, nc))
+                    queue.append((nr, nc, steps + 1))
+
+        return -1
