fb454d1273457cbbce1de00f279eb9a0d5b7e314
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-07-22 22:48:08 +0530

    Time: 41 ms (86.08%), Space: 19.1 MB (56.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,15 +1,27 @@
 class Solution:
     def fib(self, n: int) -> int:
         #BASE CASE'S
+        # if n == 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        # dp = [0]*(n+1)
+        # dp[0] = 0
+        # dp[1] = 1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # return dp[n]
+                                # time = 0(n)    space = 0(n)
         if n == 0:
             return 0
         if n == 1:
             return 1
-        dp = [0]*(n+1)
-        dp[0] = 0
-        dp[1] = 1
+        prev = 0
+        curr = 1
         for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+            prev , curr = curr , prev +  curr
+        return curr
+
+                                # TIME = 0(n)   SPACE = 0(1)
 
         
