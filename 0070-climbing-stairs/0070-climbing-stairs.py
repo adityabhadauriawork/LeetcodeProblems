8f18aab9ff792aec22c99d644c1f17638bd7b8b1
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-07-26 21:50:33 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,35 @@
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        freq={
-            0:0,
-            1:1,
-            2:2,
-            3:3
-        }
-        def f(x):
-            if x in freq:
-                return freq[x]
-            else:
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         freq={
+#             0:0,
+#             1:1,
+#             2:2,
+#             3:3
+#         }
+#         def f(x):
+#             if x in freq:
+#                 return freq[x]
+#             else:
                 
-                freq[x] = f(x-1) + f(x-2)
-                return freq[x]
-        return f(n)
+#                 freq[x] = f(x-1) + f(x-2)
+#                 return freq[x]
+#         return f(n)
+
+        # FIXED MEMORY , TABULIZATION
+class Solution:
+    def climbStairs(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        if n == 3:
+            return 3
+        prev = 2
+        curr = 3
+        for i in range(4,n+1):
+            prev,curr = curr,prev+curr
+        return curr
+
+
