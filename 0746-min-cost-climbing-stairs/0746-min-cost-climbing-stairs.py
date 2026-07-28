7f2391a0c943052e82b07557b3c0c5bab638b2f0
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-07-28 08:00:04 +0530

    Time: 1 ms (72.63%), Space: 19.2 MB (96.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -4,14 +4,16 @@ class Solution:
             return cost[0]
         if cost == []:
             return 0
-        dp = [0]*len(cost)
-        dp[0]=cost[0]
-        dp[1]=cost[1]
-        for i in range(2, len(cost)):
-            dp[i] = cost[i] + min(dp[i-1], dp[i-2])
-        return min(dp[-1],dp[-2])
-
-
-
-
-    
+        # dp = [0]*len(cost)
+        # dp[0]=cost[0]
+        # dp[1]=cost[1]
+        # for i in range(2, len(cost)):
+        #     dp[i] = cost[i] + min(dp[i-1], dp[i-2])
+        # return min(dp[-1],dp[-2])
+        prev = cost[0]
+        curr = cost[1]
+        for i in range(2,len(cost)):
+            new = cost[i]+min(prev, curr)
+            prev = curr
+            curr = new
+        return min(prev,curr)
