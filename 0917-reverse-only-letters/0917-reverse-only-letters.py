01f8e180605bc05e567bc081447b480f3034c522
Aditya Pratap Singh Bhadauria <adityabhadauriawork@gmail.com>
2026-09-04 22:22:03 +0530

    Time: 3 ms (6.75%), Space: 19.3 MB (37.77%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        a = list(s)
+        i, j = 0, len(a) - 1
+
+        while i < j:
+            if not a[i].isalpha():
+                i += 1
+            elif not a[j].isalpha():
+                j -= 1
+            else:
+                a[i], a[j] = a[j], a[i]
+                i += 1
+                j -= 1
+
+        return "".join(a)
